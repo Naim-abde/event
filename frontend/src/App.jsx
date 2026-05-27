83f6a12405d96d50b5effde6e310af7e7ce55c08
Abdessamad Naim <naimabdessamad48@gmail.com>
2026-05-27 22:47:55 +0100

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes ,Route } from 'react-router-dom'
+import './App.css'
+import Login from './page/auth/Login'
+import Register from './page/auth/Register'
+
+function App() {
+
+  return (
+    <>
+     <Routes>
+         <Route path='/login' element={<Login/>}/>
+         <Route path='/register' element={<Register/>} />
+     </Routes>
+    </>
+  )
+}
+
+export default App
